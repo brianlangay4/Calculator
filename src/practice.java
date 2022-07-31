e55e8dbe1f993e200bea4b229508a9be6f7696f7
langay <brianlangay4@gmail.com>
2022-07-31 15:56:14 +0800

    git commit -m "first commit"

---
+++ src/practice.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("enter the 1st number:");
+        //first input from user
+        Scanner number1 = new Scanner(System.in);
+        int a = number1.nextInt();
+        //second in input
+        System.out.println("input 2nd number:");
+        Scanner number2 = new Scanner(System.in);
+        int b = number2.nextInt();
+        //third input
+        System.out.println("input the method example (plus or minus):");
+        Scanner method = new Scanner(System.in);
+        String c = method.next();
+        //my conditions for the inputs
+        if (c.equals("%")){
+            System.out.println("result of:"+" "+(a%b));
+        }
+        if (c.equals("x")){
+            System.out.println("result of:"+a+"x"+b+"="+(a*b));
+        }
+        if (c.equals("/")){
+            System.out.println("result of:"+a+"/"+b+"="+(a/b));
+        }
+
+        if (c.equals("+")){
+            System.out.println("result of:"+a+"+"+b+"="+(a+b));
+        }
+        if (c.equals("-")){
+            System.out.println("the result of:"+a+"-"+b+"="+(a-b));
+        }
+
+        }
+
+
+    }
+
