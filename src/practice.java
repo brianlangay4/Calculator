5deec84d874008afd8b50933b03399fcb23ba593
langay <brianlangay4@gmail.com>
2022-08-07 20:25:00 +0800

    git commit -m "final 1"

--- src/practice.java
+++ src/practice.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class practice {
     public static void main(String[] args) {
+        //pushing new version
+        //Program starts here
+        //new version
+        System.out.println("welcome:");
         System.out.println("enter the 1st number:");
         //first input from user
         Scanner number1 = new Scanner(System.in);
@@ -12,29 +16,43 @@ public class practice {
         Scanner number2 = new Scanner(System.in);
         int b = number2.nextInt();
         //third input
-        System.out.println("input the method example (plus or minus):");
-        Scanner method = new Scanner(System.in);
-        String c = method.next();
-        //my conditions for the inputs
-        if (c.equals("%")){
-            System.out.println("result of:"+" "+(a%b));
-        }
-        if (c.equals("x")){
-            System.out.println("result of:"+a+"x"+b+"="+(a*b));
-        }
-        if (c.equals("/")){
-            System.out.println("result of:"+a+"/"+b+"="+(a/b));
-        }
-
-        if (c.equals("+")){
-            System.out.println("result of:"+a+"+"+b+"="+(a+b));
-        }
-        if (c.equals("-")){
-            System.out.println("the result of:"+a+"-"+b+"="+(a-b));
-        }
-
-        }
-
-
+        boolean rep = true;
+        do {
+            try {
+                System.out.println("input the method example (plus as(+) or minus(-)):");
+                Scanner method = new Scanner(System.in);
+                String c = method.next();
+                //my conditions for the inputs
+                if (c.equals("%")) {
+                    System.out.println("result of:" + " " + (a % b));
+                    break;
+                }
+                if (c.equals("x")) {
+                    System.out.println("result of:" + a + "x" + b + "=" + (a * b));
+                    break;
+                }
+                if (c.equals("/")) {
+                    System.out.println("result of:" + a + "/" + b + "=" + (a / b));
+                    break;
+                }
+                if (c.equals("+")) {
+                    System.out.println("result of:" + a + "+" + b + "=" + (a + b));
+                    break;
+                }
+                if (c.equals("-")) {
+                    System.out.println("the result of:" + a + "-" + b + "=" + (a - b));
+                    break;
+                } else {
+                    System.out.println("invalid method identified");
+                    System.out.println("please read this");
+                    System.out.println("for multiplication use(x)");
+                    System.out.println("for division use(/)");
+                }
+            } catch (ArithmeticException A){
+                System.out.println("first value can not be / by 0");
+                System.out.println("program Ends here re run");
+                break;
+            }
+        }while (rep) ;
     }
-
+}
